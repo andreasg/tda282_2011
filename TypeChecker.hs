0fd4413be7957b8465ac9d15c1277dc31470f07f
Andreas Granstrom <andreas.granstrom@gmail.com>
2011-03-29 21:59:24 +0200

    pillat lite...

--- TypeChecker.hs
+++ TypeChecker.hs
@@ -1,6 +1,5 @@
 module TypeChecker where
 
-
 -- BNF Converter imports
 import AbsJavalette
 import AbsJavalette
@@ -14,7 +13,6 @@ import Control.Monad.Reader
 
 -- Environment, pair each identifier with it's type
 type Env = [(Ident,Type)]
-
 type State a = ReaderT Env Err a
 
 -- Standard Environment, containing all 'std' functions of Javalette
@@ -27,11 +25,13 @@ stdEnv = [(Ident "printInt",    Fun Void [Int])
 
 -- Check if a program is type correct
 typecheck :: Program -> Err Bool
-typecheck p@(Program fs) = runReaderT (buildSig fs) stdEnv >>= 
-                           runReaderT (mapM checkTopDef fs) >> if returnCheck fs 
-                                                                then return True 
-                                                                else fail "all non-void must return"
+typecheck (Program fs) = runReaderT (buildSig fs) stdEnv  >>= 
+                         runReaderT (mapM checkTopDef fs) >> 
+                         if returnCheck fs 
+                           then return True 
+                           else fail "all non-void must return"
 
+-- build type-signatures for functions
 buildSig :: [TopDef] -> State Env
 buildSig []                     = ask
 buildSig (FnDef t id args _:xs) = do
@@ -44,92 +44,76 @@ buildSig (FnDef t id args _:xs) = do
 
 
 checkTopDef :: TopDef -> State Type
-checkTopDef (FnDef t id args (Block b)) = do
-  local (\x -> map f args ++ x) (typeStmt b t)
+checkTopDef (FnDef t id args (Block b)) = local (\x -> map f args++x) (typeStmt b t)
     where f (Arg t i) = (i,t)
 
 
 typeStmt :: [Stmt] -> Type -> State Type
 typeStmt []                   rt = return Void
 typeStmt (Empty:s)            rt = typeStmt s rt
-typeStmt (BStmt (Block b):s)  rt = do
-  local id (typeStmt b rt)
-  typeStmt s rt
-typeStmt (Decl t is:s)        rt = do
-  ids <- mapM (checkItem t) is
-  local (addVars t ids) (typeStmt s rt)
-typeStmt (Ass id e:s)         rt = do
-  t <- typeExpr e
-  cs <- ask
-  case lookup id cs of
-    Nothing -> fail $ "Variable " ++ show id ++ " does not exist"
-    Just t' -> if t'==t
-               then typeStmt s rt
-               else fail $ "Expression " ++ show e ++ " has the wrong type"
-typeStmt (Incr id:s)          rt = do
-  inferNumId id
-  typeStmt s rt
-typeStmt (Decr id:s)          rt = do
-  inferNumId id
-  typeStmt s rt
-typeStmt (Ret e:s)            rt = do
-  local id (typeStmt s rt)
-  t <- typeExpr e
-  if t == rt
-    then return t
-    else fail $ show t ++ " is not of return type " ++ show rt
+typeStmt (BStmt (Block b):s)  rt = local id (typeStmt b rt) >> typeStmt s rt
+typeStmt (Incr id:s)          rt = inferNumId id >> typeStmt s rt
+typeStmt (Decr id:s)          rt = inferNumId id >> typeStmt s rt
+typeStmt (SExp e:s)           rt = typeExpr e    >> typeStmt s rt
+typeStmt (Decl t is:s)        rt = do ids <- mapM (checkItem t) is 
+                                      local (addVars t ids) (typeStmt s rt)
+typeStmt (Cond e s':s)        rt = do inferBool e
+                                      local id (typeStmt [s'] rt)
+                                      typeStmt s rt
+typeStmt (CondElse e s0 s1:s) rt = do inferBool e
+                                      local id (typeStmt [s0] rt)
+                                      local id (typeStmt [s1] rt)         
+                                      typeStmt s rt
+typeStmt (While e s':s)       rt = do inferBool e
+                                      local id (typeStmt [s'] rt)
+                                      typeStmt s rt
+typeStmt (Ass id e:s)         rt = do t <- typeExpr e
+                                      cs <- ask
+                                      case lookup id cs of
+                                       Nothing -> fail $ "Variable " ++ show id ++ " does not exist"
+                                       Just t' -> if t'==t
+                                                   then typeStmt s rt
+                                                   else fail $ "Expression " ++ show e 
+                                                                             ++ " has the wrong type"
+typeStmt (Ret e:s)            rt = do local id (typeStmt s rt)
+                                      t <- typeExpr e
+                                      if t == rt
+                                        then return t
+                                        else fail $ show t ++ " is not of return type " ++ show rt
 typeStmt (VRet:s)             rt | rt == Void = return Void
                                  | otherwise  = fail $ "Return type not " ++ show rt
-typeStmt (Cond e s':s)        rt = do
-  inferBool e
-  local id (typeStmt [s'] rt)
-  typeStmt s rt
-typeStmt (CondElse e s0 s1:s) rt = do
-  inferBool e
-  local id (typeStmt [s0] rt)
-  local id (typeStmt [s1] rt)
-  typeStmt s rt
-typeStmt (While e s':s)       rt = do
-  inferBool e
-  local id (typeStmt [s'] rt)
-  typeStmt s rt
-typeStmt (SExp e:s)           rt = do
-  typeExpr e
-  typeStmt s rt
 
 
 typeExpr :: Expr -> State Type
-typeExpr (EVar id)      = typeIdent id
-typeExpr (ELitInt _)    = return Int
-typeExpr (ELitDoub _)   = return Doub
-typeExpr ELitTrue       = return Bool
-typeExpr ELitFalse      = return Bool
-typeExpr (EString _)    = return Void
-typeExpr (Neg e)        = inferNum e
-typeExpr (Not e)        = inferBool e
-typeExpr (EMul e0 _ e1) = inferNumBin e0 e1
-typeExpr (EAdd e0 _ e1) = inferNumBin e0 e1
-typeExpr (ERel e0 op e1) = do
-  t0 <- typeExpr e0
-  t1 <- typeExpr e1
-  if t0 == t1
-    then return Bool
-    else fail "incompatable types in relop"
-typeExpr (EAnd e0 e1)   = inferBoolBin e0 e1
-typeExpr (EOr e0 e1)    = inferBoolBin e0 e1
-typeExpr (EApp id args) = do
-  cs <- ask
-  argTs <- mapM typeExpr args
-  case lookup id cs of
-    Just (Fun t ts) -> if ts == argTs
-                       then return t
-                       else fail $ show id ++ " gets wrong arguments"
-    _               -> fail $ show id ++ " does not exist"
-
+typeExpr (EVar id)       = typeIdent id
+typeExpr (ELitInt _)     = return Int
+typeExpr (ELitDoub _)    = return Doub
+typeExpr ELitTrue        = return Bool
+typeExpr ELitFalse       = return Bool
+typeExpr (EString _)     = return Void
+typeExpr (Neg e)         = inferNum e
+typeExpr (Not e)         = inferBool e
+typeExpr (EMul e0 _ e1)  = inferNumBin e0 e1
+typeExpr (EAdd e0 _ e1)  = inferNumBin e0 e1
+typeExpr (EAnd e0 e1)    = inferBoolBin e0 e1
+typeExpr (EOr e0 e1)     = inferBoolBin e0 e1
+typeExpr (ERel e0 op e1) = do t0 <- typeExpr e0
+                              t1 <- typeExpr e1
+                              if t0 == t1
+                                then return Bool
+                                else fail "incompatable types in relop"
+typeExpr (EApp id args) = do cs <- ask
+                             argTs <- mapM typeExpr args
+                             case lookup id cs of
+                               Just (Fun t ts) -> if ts == argTs
+                                                    then return t
+                                                    else fail $ show id ++ " gets wrong arguments"
+                               _               -> fail $ show id ++ " does not exist"
 
 
 addVars :: Type -> [Ident] -> Env -> Env
 addVars t is e = foldr (:) e (zip is (repeat t))
+
 
 checkItem :: Type -> Item -> State Ident
 checkItem _ (NoInit id) = do
@@ -145,6 +129,7 @@ checkItem t (Init id e) = do
                then return id
                else fail $ "Expression " ++ show e ++ " has the wrong type"
     _       -> fail $ "Variable " ++ show id ++ " already exists"
+
 
 typeIdent :: Ident -> State Type
 typeIdent id = do
@@ -184,10 +169,7 @@ inferBool e = do
     else fail $ "Expression " ++ show e ++ " is not a boolean"
 
 inferBoolBin :: Expr -> Expr -> State Type
-inferBoolBin e0 e1 = do
-  inferBool e0
-  inferBool e1
-
+inferBoolBin e0 e1 = inferBool e0 >> inferBool e1
 
 -- check if a program returns correctly
 returnCheck :: [TopDef] -> Bool
